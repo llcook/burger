09a9ffce451f185080357fef91ad310c41aa4789
watercolorhorse <29869241+llcook@users.noreply.github.com>
2019-01-10 19:21:40 -0700

    burger names now display on page

--- config/orm.js
+++ config/orm.js
@@ -1,14 +1,4 @@
-// Import (require) `connection.js` into `orm.js`
-// NOTE: CATSAPP EXAMPLE SHOWS THIS CONNECTION AS SUCH
-// BUT orm.js AND connection.js ARE IN THE SAME DIRECTORY -- WHY THIS SYNTAX?
 var connection = require("../config/connection.js");
-
-////////////////////////////////////////////////////
-////////////////////////////////////////////////////
-////////////////////////////////////////////////////
-
-// FROM CATSAPP
-// DON'T REALLY GET WHAT THIS IS
 
 function printQuestionMarks(num) {
     var arr = [];
@@ -43,15 +33,7 @@ function printQuestionMarks(num) {
     return arr.toString();
   }
 
-////////////////////////////////////////////////////
-////////////////////////////////////////////////////
-////////////////////////////////////////////////////
-
 //    * In the `orm.js` file, create the methods that will execute the necessary MySQL commands in the controllers. These are the methods you will need to use in order to retrieve and store data in your database.
-
-//      * `selectAll()`
-//      * `insertOne()`
-//      * `updateOne()`
 
 var orm = {
     selectAll: function (tableInput, cb) {
