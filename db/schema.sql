988ed980a1ceffdd1471485aa6a28c136ed17c3b
watercolorhorse <29869241+llcook@users.noreply.github.com>
2019-01-10 20:14:46 -0700

    updated styles - removed default boolean from schema - updated delete button text to X - change ul to p

--- db/schema.sql
+++ db/schema.sql
@@ -7,6 +7,6 @@ USE burgers_db;
 CREATE TABLE burgers (
 	id INT AUTO_INCREMENT NOT NULL,
     burger_name VARCHAR(50) NOT NULL,
-    devoured BOOLEAN DEFAULT false,
+    devoured BOOLEAN,
     PRIMARY KEY (id)
 );
