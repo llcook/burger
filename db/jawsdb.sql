d9afea4f71add4f028f3127fa69e16b817c04188
watercolorhorse <29869241+llcook@users.noreply.github.com>
2019-01-10 21:05:42 -0700

    created jawsDB schema per heroku deployment tutorial - it did not instruct to save the schema file, but doing it anyway

---
+++ db/jawsdb.sql
@@ -0,0 +1,7 @@
+CREATE TABLE burgers (
+	id INT AUTO_INCREMENT NOT NULL,
+    burger_name VARCHAR(50) NOT NULL,
+    devoured BOOLEAN,
+    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    PRIMARY KEY (id)
+);
